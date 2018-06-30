d49f47ad5c797a8ee613c4f8ab7790d9fdf1c198
涂锐齐 <trq@192.168.0.108>
2018-06-30 22:46:14 +0800

    python2 >> python3

--- 汉诺塔.py
+++ 汉诺塔.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-#coding=utf
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 def answer_z(x, y):#已知起始柱子与终点柱子求解空闲柱子
     all = ('A','B','C')
     for a in all :
@@ -11,9 +11,9 @@ def answer_z(x, y):#已知起始柱子与终点柱子求解空闲柱子
 def move_A_C(n,x,y):#在x柱子上有n层圆盘，求解移动到y柱子上的步骤
 	z = answer_z(x,y)
 	if n == 1:
-		print '%s --> %s' %(x,y)
+		print ('%s --> %s' %(x,y))
 	else:
 		move_A_C(n-1,x,z)
-		print '%s --> %s' %(x,y)
+		print ('%s --> %s' %(x,y))
 		move_A_C(n-1,z,y)
 move_A_C(10,'A','C')
