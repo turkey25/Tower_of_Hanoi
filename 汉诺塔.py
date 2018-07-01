7f1bb08265a25723b5f3cd139d179ace043935b4
涂锐齐 <trq@192.168.0.108>
2018-07-01 15:27:33 +0800

    Simplify code logic

--- 汉诺塔.py
+++ 汉诺塔.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-def answer_z(x, y):#已知起始柱子与终点柱子求解空闲柱子
-    all = ('A','B','C')
-    for a in all :
-        if a == x:
-            continue
-        if a == y:
-            continue
-        return a
-def move_A_C(n,x,y):#在x柱子上有n层圆盘，求解移动到y柱子上的步骤
-	z = answer_z(x,y)
+def move_A_C(n,a,b,c):#在x柱子上有n层圆盘，求解移动到y柱子上的步骤
 	if n == 1:
-		print ('%s --> %s' %(x,y))
+		print ('%s --> %s' %(a,c))
 	else:
-		move_A_C(n-1,x,z)
-		print ('%s --> %s' %(x,y))
-		move_A_C(n-1,z,y)
-move_A_C(10,'A','C')
+		move_A_C(n-1,a,c,b)
+		print ('%s --> %s' %(a,c))
+		move_A_C(n-1,b,a,c)
+move_A_C(10,'A','B','C')
